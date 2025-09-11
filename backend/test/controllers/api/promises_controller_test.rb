a5d7cea2137aa9955c414e2a38609adef8cae212
compile425 <yamamoto425k@gmail.com>
2025-09-11 17:17:48 +0900

    テストの設定変更

--- backend/test/controllers/api/promises_controller_test.rb
+++ backend/test/controllers/api/promises_controller_test.rb
@@ -2,7 +2,9 @@ require "test_helper"
 
 class Api::PromisesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get api_promises_url
+    user = users(:one) if defined?(users)
+    token = ApplicationController.new.encode_token(user_id: user&.id || 1)
+    get api_promises_url, headers: { "Authorization" => "Bearer #{token}" }
     assert_response :success
   end
 end
