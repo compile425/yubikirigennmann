ccfc6824150ae4edc4034dac23b97986bd8a9cdd
compile425 <yamamoto425k@gmail.com>
2025-09-11 17:13:46 +0900

    パスへルパの修正

--- backend/test/controllers/api/promises_controller_test.rb
+++ backend/test/controllers/api/promises_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class Api::PromisesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get api_promises_index_url
+    get api_promises_url
     assert_response :success
   end
 end
