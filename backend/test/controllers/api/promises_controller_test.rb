75811c8f39db998e72505e0c4a8fb6264a6b8bf7
compile425 <yamamoto425k@gmail.com>
2025-09-11 19:53:09 +0900

    エラー修正

--- backend/test/controllers/api/promises_controller_test.rb
+++ backend/test/controllers/api/promises_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class Api::PromisesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    user = users(:one)
+    user = User.create!(email: "test@example.com", name: "Test User")
     payload = { user_id: user.id, exp: 24.hours.from_now.to_i }
     secret = Rails.application.secret_key_base
     token = JWT.encode(payload, secret)
