f7b57bdb2d78c5dde3e1e47bedfaba494c9ff583
compile425 <yamamoto425k@gmail.com>
2025-09-21 17:15:14 +0900

    デプロイテスト

--- backend/app/controllers/health_controller.rb
+++ backend/app/controllers/health_controller.rb
@@ -1,4 +1,6 @@
 class HealthController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show]
+
   def show
     render json: { status: 'ok', timestamp: Time.current }
   end
