b3f2d6b8fe5d78aa36b27c6fa03b107d901eeb94
compile425 <yamamoto425k@gmail.com>
2025-09-30 11:45:40 +0900

    ログインエラー修正

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,18 @@
+# backend/config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins "https://yubikirigenman.com", "https://www.yubikirigenman.com", "http://localhost:3000", "http://127.0.0.1:3000"
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  if Rails.env.development?
+    allow do
+      origins %r{\Ahttp://localhost:\d+\z}, %r{\Ahttp://127\.0\.0\.1:\d+\z}
+      resource "*", headers: :any,
+               methods: [:get, :post, :put, :patch, :delete, :options, :head],
+               credentials: true
+    end
+  else
+    allow do
+      origins "https://yubikirigenman.com", "https://www.yubikirigenman.com"
+      resource "*", headers: :any,
+               methods: [:get, :post, :put, :patch, :delete, :options, :head],
+               credentials: true
+    end
   end
 end
